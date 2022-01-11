2ee22dd1e8929dafb08c601800d46e8dc32a2ff9
Grand Oeuvre <91910447+Eliouslehecker@users.noreply.github.com>
2022-01-11 17:49:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ document.getElementById('form')
  .addEventListener('submit', function(event) {
    event.preventDefault();
 
-   btn.value = 'Sending...';
+   btn.value = 'Envoi...';
 
    const serviceID = 'default_service';
    const templateID = 'template_ycfgbfi';
 
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-      btn.value = 'Send Email';
+      btn.value = 'Envoyer';
       alert('Sent!');
     }, (err) => {
-      btn.value = 'Send Email';
+      btn.value = 'Envoyer';
       alert(JSON.stringify(err));
     });
 });
